cbab1b65da5aa86fbe039f0800b6f90f4ed5d5c9
llister15 <llister@wonkasoft.com>
2017-10-25 17:36:22 -0700

    added MailChimp API

---
+++ src/php/mailchimp-config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * author Wonkasoft
+ * author uri https://wonkasoft.com
+ * source https://github.com/Wonkasoft/MailChimpAPI
+ * @package MailChimp API
+ * @since 1.0.0
+ * 
+ */
+// Put your MailChimp API and List ID here
+$api_key = 'b041c292a51bda319f461e6ffb5a915b-us16';
+$list_id = 'b167b4aacf';
