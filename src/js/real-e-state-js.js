e1d188e4df152a4856e9a18b1601f8441bba0195
MrLister <rlister@wonkasoft.com>
2017-11-21 02:05:49 -0800

    MrLister 11-21-17 2:05am

--- src/js/real-e-state-js.js
+++ src/js/real-e-state-js.js
@@ -1,21 +1,32 @@
 ( function() {
 'use strict';
 	var foot_height = document.getElementById('e-state-footer').clientHeight;
-	window.onload = get_footer_height();
 
 	window.addEventListener( 'resize', get_footer_height );
 
 	function get_footer_height() {
+
 		foot_height = document.getElementById('e-state-footer').clientHeight;
 		document.getElementById('main').style.paddingBottom = foot_height + 'px';
+
 	}
 
 	window.onload = function () {
-		var featured_span = document.getElementsByClassName( 'simple_list' )[0].getElementsByTagName( 'span' )[0];
-		var featured_img = featured_span.getElementsByTagName( 'img' )[0];
-		featured_span.style.width = '100%';
-		featured_span.style.height = '100%';
-		featured_img.style.width = '100%';
+
+		get_footer_height();
+
+		if ( document.querySelector( '.simple_list' ) ) {
+
+			var featured_span = document.getElementsByClassName( 'simple_list' )[0].getElementsByTagName( 'span' );
+			var featured_img = '';
+			for (var i = 0; i < featured_span.length; i++) {
+				featured_img = featured_span[i].getElementsByTagName( 'img' )[0];
+				featured_span[i].style.width = '100%';
+				featured_span[i].style.height = '100%';
+				featured_img.style.width = '100%';
+			}
+
+		}
 	};
 	
 })();
