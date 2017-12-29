1f5c67b8a775aa16d9171fb1fe35d1ed211cf791
MrLister <m_r_lister@yahoo.com>
2017-12-29 15:52:45 -0800

    MrLister 12-29-17 3:52pm

---
+++ single.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying all single posts
+ * 
+ * @package real-e-state
+ * @version  1.0.0 
+ * @since  1.0.0 
+ */
+
+get_header(); ?>
+<main id="main">
+	<?php
+	if ( locate_template('template-parts/navigation/content-page.php') != '' ) {
+		get_template_part( 'template-parts/navigation/content', 'page' );
+	} else { 
+		// If not home page parse this
+		?>
+		<section id="under-header" class="hidden-xs">
+			<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12">
+
+				</div> <!-- /col-xs-12 -->
+			</div> <!-- /row -->
+			</div> <!-- /container-fluid -->
+	</section> <!-- #under-header -->
+		<section id="content-section">
+			<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/post/single', 'post' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+			</div> <!-- /col-xs-12 -->
+			</div> <!-- /row -->
+			</div> <!-- /container-fluid -->
+		</section> <!-- /#content-section -->
+
+		<?php 
+	} ?>
+</main><!-- /#main -->
+<?php
+get_footer();
--- src/js/real-e-state-js.js
+++ src/js/real-e-state-js.js
@@ -1,14 +1,5 @@
 ( function() {
 'use strict';
-	var foot_height = document.getElementById('e-state-footer').clientHeight;
-	window.onload = get_footer_height();
-
-	window.addEventListener( 'resize', get_footer_height );
-
-	function get_footer_height() {
-		foot_height = document.getElementById('e-state-footer').clientHeight;
-		document.getElementById('main').style.paddingBottom = foot_height + 'px';
-	}
 
 	window.onload = function () {
 		var featured_span = document.getElementsByClassName( 'simple_list' )[0].getElementsByTagName( 'span' )[0];
