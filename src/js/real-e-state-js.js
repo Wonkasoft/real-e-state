53ec17ad2577929b3785f5efbb3783c4a4b508e2
MrLister <rlister@wonkasoft.com>
2018-01-18 21:41:49 -0800

    MrLister fixing some styles on the home page. also some js updates.

--- src/js/real-e-state-js.js
+++ src/js/real-e-state-js.js
@@ -1,5 +1,5 @@
 ( function() {
-'use strict';
+"use strict";
 	window.onload = function () {
 
 		if ( document.querySelector( '.simple_list' ) ) {
@@ -13,7 +13,46 @@
 			}
 		// endif
 		} 
+
+		if ( document.getElementsByTagName('body')[0].classList.contains( 'home' ) ) {
+			listing_li_wraps();
+			listing_img_fix();
+		}
 	// end function
 	};
+
+	window.onresize = function() {
+		if ( document.getElementsByTagName('body')[0].classList.contains( 'home' ) ) {
+			listing_img_fix();
+		}
+	};
+
+	function listing_li_wraps() {
+		var li_select = document.querySelectorAll( 'div.current-listings > .simple_list > li' );
+		var li_get, li_innerwraps;
+		for (var i = 0; i < li_select.length; i++) {
+			li_get = li_select[i].innerHTML;
+			li_innerwraps = '<div class="listing-wrap"><div class="listing-modules">' + li_get + '</div></div>';
+			li_select[i].innerHTML = li_innerwraps;
+		}
+	}
+
+	function listing_img_fix() {
+			var listing_img;
+			listing_img = document.querySelectorAll( '.left_section .loaded' );
+			if ( window.innerWidth <= 991 ) {
+				for (var i = 0; i < listing_img.length; i++) {
+					listing_img[i].removeAttribute( 'height' );
+					listing_img[i].style.width = '100%' ;
+					listing_img[i].style.height = 'auto' ;
+				}
+			} else {
+				for (var i = 0; i < listing_img.length; i++) {
+					listing_img[i].setAttribute( 'height', '175' );
+					listing_img[i].style.width = '100%' ;
+					listing_img[i].style.height = '175px' ;
+				}
+			}
+	}
 	
 })();
